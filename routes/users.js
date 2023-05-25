2604478abc5568407b034dda8b457179ba35725f
Muhammad Rasi M S <muhammadrasi0@gmail.com>
2023-05-25 17:27:00 +0530

    form submission added, contact page updated

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer');
 const userHelpers = require('../helpers/user-helpers');
 
 router.use("/", (req, res, next) => {
@@ -39,9 +40,87 @@ router.get("/post-press", async(req, res) => {
   res.render("users/post-press", { user: true, postpress });
 });
 
-router.get("/getaqoute", (req, res) => {
+router.get("/getaqoute", async(req, res) => {
   res.render("users/getaqoute", { user: true});
-})
+});
 
+
+router.post("/getaqoute", (req, res) => {
+  let from = req.body.from;
+  let to = req.body.to;
+  let name = req.body.name;
+  let phone = req.body.phone;
+  let message = req.body.message;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "yoursiteuser@gmail.com",
+      pass: "gkfqsbchvbqunxou",
+    },
+  });
+
+  let mailOptions = {
+    from: from,
+    to: to,
+    subject: "Quote Request",
+    html: `
+      <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
+        <h2 style="color: #333;">Quote Request</h2>
+        <p style="font-size: 16px;">Email: ${from}</p>
+        <p style="font-size: 16px;">Name: ${name}</p>
+        <p style="font-size: 16px;">Phone: ${phone}</p>
+        <p style="font-size: 16px;">Message: ${message}</p>
+      </div>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error){
+      console.log(error);
+    } else {
+      console.log("Email send:" + info.response);
+    }
+    res.redirect('/getaqoute', "/getaqoute", { user: true });
+  })
+});
+
+router.post("/contact", (req, res) => {
+  let from = req.body.from;
+  let to = req.body.to;
+  let name = req.body.name;
+  let message = req.body.message;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "yoursiteuser@gmail.com",
+      pass: "gkfqsbchvbqunxou",
+    },
+  });
+
+  let mailOptions = {
+    from: from,
+    to: to,
+    subject: "Quote Request",
+    html: `
+      <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
+        <h2 style="color: #333;">Quote Request</h2>
+        <p style="font-size: 16px;">Email: ${from}</p>
+        <p style="font-size: 16px;">Name: ${name}</p>
+        <p style="font-size: 16px;">Message: ${message}</p>
+      </div>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email send:" + info.response);
+    }
+    res.redirect("/contact", "/contact", { user: true });
+  });
+});
 
 module.exports = router;
